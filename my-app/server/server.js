2a0c9b53adcebd55682e67772faa671eae40c14f
Hristo <generatorfox@gmail.com>
2024-08-24 20:51:38 +0300

    add login function and SQLite db

---
+++ my-app/server/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const sqlite3 = require("sqlite3").verbose();
+
+app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+app.use(express.json({ limit: "10mb" }));
+
+let db = new sqlite3.Database("credentials.db", (err) => {
+  if (err) {
+    console.error("Failed to connect to the database:", err.message);
+    process.exit(1); // Exit the application if the database connection fails
+  }
+  console.log("Connected to the database.");
+});
+
+app.post("/validatePassword", (req, res) => {
+  const { username, password } = req.body;
+  console.log("Received username:", username);
+  console.log("Received password:", password);
+  const query = `SELECT * FROM credentials WHERE username = ? AND password = ?`;
+
+  db.get(query, [username, password], (err, row) => {
+    if (err) {
+      console.error("Error executing query:", err.message);
+      res.status(500).send({ error: "Internal Server Error" });
+      return;
+    }
+    if (row) {
+      console.log("User found:", row);
+      res.send({ validation: true });
+    } else {
+      console.log("User not found or incorrect password");
+      res.send({ validation: false });
+    }
+  });
+});
+
+app.listen(3001, () => console.log("Listening at port 3001"));
---
+++ my-app/src/components/LoginForm.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { useState } from "react";
+
+export default function LoginForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogged, setIsLogged] = useState(false);
+  const onFinish = (e) => {
+    e.preventDefault();
+    console.log("done");
+
+    axios
+      .post("http://localhost:3001/validatePassword", { username, password })
+      .then((res) => {
+        if (res.data.validation) {
+          alert("Your password is correct");
+          setIsLogged(true);
+        } else {
+          alert("Your password is not correct");
+        }
+      });
+    console.log(isLogged);
+  };
+  return (
+    <div>
+      <form onSubmit={onFinish}>
+        <input
+          name="username"
+          placeholder="Name"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Log in</button>
+      </form>
+    </div>
+  );
+}
